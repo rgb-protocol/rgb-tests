b940431fab45887b50b20a56f6bc0fdec5c7c020
Nicola Busanello <nicola.busanello@gmail.com>
2025-04-16 17:19:41 +0200

    submodules-rev.sh improvements
     * filter altered submodules
     * add fetch command

--- submodules-rev.sh
+++ submodules-rev.sh
@@ -40,145 +40,207 @@ _war() {
     printf "${ORANGE}WARNING: %s${NC}\n" "$@"
 }
 
-# CLI handling
+_init() {
+    # change to script directory (project root)
+    pushd "$DIR" >/dev/null || exit 1
+
+    # get submodule list
+    if ! [ -r .gitmodules ]; then
+        _die "project has no git submodules"
+    fi
+    SUBS=$(git submodule | awk '{print $2}' | grep -v altered_submodules)
+}
+
+_cleanup() {
+    # go back to calling directory
+    while [ "$(dirs -p | wc -l)" -gt 1 ]; do
+        popd >/dev/null || exit 1
+    done
+    exit 0
+}
+
+# help functions
+change_help() {
+    echo "$0 change <options>"
+    echo ""
+    echo "options:"
+    echo "    -b --branch <branch>  change to the specified branch"
+    echo "    -f --fetch            fetch provided remote (or all if none specified)"
+    echo "    -h --help             show this message help"
+    echo "    -r --remote <remote>  remote to be used"
+    echo "    -t --tag    <tag?     change to the specified tag"
+}
+
+fetch_help() {
+    echo "$0 fetch [options]"
+    echo ""
+    echo "options:"
+    echo "    -h --help             show this message help"
+    echo "    -r --remote <remote>  remote to be used"
+}
+
+# command functions
+change() {
+    # option handling
+    while [ -n "$1" ]; do
+        case $1 in
+            -h | --help)
+                change_help
+                exit 0
+                ;;
+            -b | --branch)
+                BRANCH="$2"
+                shift
+                ;;
+            -f | --fetch)
+                FETCH=1
+                ;;
+            -r | --remote)
+                REMOTE="$2"
+                shift
+                ;;
+            -t | --tag)
+                TAG="$2"
+                shift
+                ;;
+            *)
+                change_help
+                _die "unsupported option \"$1\""
+                ;;
+        esac
+        shift
+    done
+
+    # check branch/tag combination makes sense
+    if [ -z "$BRANCH" ] && [ -z "$TAG" ]; then
+        change_help
+        _die "please specify a branch or a tag to switch to"
+    fi
+    if [ -n "$BRANCH" ] && [ -n "$TAG" ]; then
+        change_help
+        _die "please either specify a branch or a tag, not both"
+    fi
+
+    # optionally add the remote
+    if [ -n "$REMOTE" ]; then
+        if [ -n "$BRANCH" ]; then
+            BRANCH="$REMOTE/$BRANCH"
+        else
+            change_help
+            _die "specifying a remote only makes sense if a branch is specified as well"
+        fi
+    fi
+
+    # update submodule revs
+    for sub in $SUBS; do
+        # init
+        _head "---- submodule: $sub"
+        while [ "$(dirs -p | wc -l)" -gt 2 ]; do
+            popd >/dev/null || exit 1
+        done
+        pushd "$DIR/$sub" >/dev/null || exit 1
+        # make sure the remote exists, if specified
+        if [ -n "$REMOTE" ]; then
+            if ! git remote | grep -xq "$REMOTE"; then
+                _war "remote \"$REMOTE\" not found"
+                continue
+            fi
+        fi
+        # update repo
+        if [ "$FETCH" = 1 ]; then
+            if [ -n "$REMOTE" ]; then
+                git fetch "$REMOTE"
+            else
+                git fetch --all
+            fi
+        fi
+        # branch case
+        if [ -n "$BRANCH" ]; then
+            # check if specified branch exists
+            if ! git branch -r | grep -wq "$BRANCH"; then
+                _war "branch \"$BRANCH\" not found"
+                continue
+            fi
+            # checkout the specified branch
+            git checkout "$BRANCH"
+            # pull latest changes
+            git pull
+        fi
+        # tag case
+        if [ -n "$TAG" ]; then
+            if ! git tag | grep -xq "$TAG"; then
+                _war "tag \"$TAG\" not found"
+                continue
+            fi
+            # checkout the specified tag
+            git checkout "$TAG"
+        fi
+    done
+}
+
+fetch() {
+    # option handling
+    while [ -n "$1" ]; do
+        case $1 in
+            -h | --help)
+                fetch_help
+                exit 0
+                ;;
+            -r | --remote)
+                REMOTE="$2"
+                shift
+                ;;
+            *)
+                fetch_help
+                _die "unsupported option \"$1\""
+                ;;
+        esac
+        shift
+    done
+
+    # remote handling
+    [ -n "$REMOTE" ] || REMOTE="--all"
+
+    # fetch submodules
+    # shellcheck disable=SC2016
+    git submodule foreach \
+        "git fetch $REMOTE || :" # ignore failures (i.e. missing remote)
+}
+
 help() {
     echo "$0 <command>"
     echo ""
     echo "commands:"
-    echo "    status         show all submodule revs"
-    echo "    change         change submodule revs"
-    echo "    help           show this help message"
-}
-
-change_help() {
-    echo "$0 change [-h] [-f] [-r <remote>] (-b) <branch>"
-    echo ""
-    echo "options:"
-    echo "    -b --branch    change to the specified branch"
-    echo "    -f --fetch     fetch provided remote (or all if none specified)"
-    echo "    -r --remote    remote to be used"
-    echo "    -t --tag       change to the specified tag"
+    echo "    change <options>  change submodule revs"
+    echo "    fetch  <options>  fetch all submodules"
+    echo "    help              show this help message"
+    echo "    status            show all submodule revs"
 }
 
 status() {
+    # option handling
+    [ -n "$1" ] && _die "the status command takes no arguments"
+
+    # shellcheck disable=SC2016
     git submodule foreach --quiet \
         'echo "" $(git rev-list -n1 HEAD) $sm_path $(git describe --all | sed "s,^\(tags\|remotes\|heads\)/,(,;s/$/)/")'
 }
 
+# initialize and setup cleanup on exit
 if [ -z "$1" ]; then
     help
     _die "please provide a command"
 fi
+_init
+trap _cleanup EXIT HUP INT QUIT TERM
 
-case $1 in
-    help)
-        help
-        exit 0
-        ;;
-    status)
-        status
-        exit 0
-        ;;
-    change)
-        shift
-        ;;
-    *)
-        help
-        _die "unsupported command \"$1\""
-        ;;
-esac
-
-while [ -n "$1" ]; do
-    case $1 in
-        -h | --help)
-             change_help
-             exit 0
-             ;;
-        -b | --branch)
-            BRANCH="$2"
-            shift
-            ;;
-        -f | --fetch)
-            FETCH=1
-            ;;
-        -r | --remote)
-            REMOTE="$2"
-            shift
-            ;;
-        -t | --tag)
-            TAG="$2"
-            shift
-            ;;
-        *)
-            change_help
-            _die "unsupported option \"$1\""
-            ;;
-    esac
+# command handling
+VALID_CMDS=("change" "fetch" "help" "status")
+CMD="$1"
+if [ -n "${VALID_CMDS[$CMD]}" ]; then
     shift
-done
-
-# check a branch or tag have been specified
-if [ -z "$BRANCH" ] && [ -z "$TAG" ]; then
-    change_help
-    _die "please specify a branch or a tag to switch to"
+    $CMD "$@"
+else
+    help
+    _die "unsupported command \"$1\""
 fi
-if [ -n "$BRANCH" ] && [ -n "$TAG" ]; then
-    _die "please either specify a branch or a tag, not both"
-fi
-
-# optionally add the remote
-if [ -n "$REMOTE" ]; then
-    BRANCH="$REMOTE/$BRANCH"
-fi
-
-# change to the script directory (project root)
-pushd "$DIR" >/dev/null || exit 1
-
-# get list of submodules
-if ! [ -r .gitmodules ]; then
-    _die "project has no git submodules"
-fi
-SUBS=$(awk -F '"' '/submodule/ {print $2}' .gitmodules)
-
-# update submodule revs
-for sub in $SUBS; do
-    _head "---- submodule: $sub"
-    cd "$DIR/$sub" || exit 1
-    # make sure the remote exists, if specified
-    if [ -n "$REMOTE" ]; then
-        if ! git remote | grep -xq "$REMOTE"; then
-            _war "remote \"$REMOTE\" not found"
-            continue
-        fi
-    fi
-    # update repo
-    if [ "$FETCH" = 1 ]; then
-        if [ -n "$REMOTE" ]; then
-            git fetch "$REMOTE"
-        else
-            git fetch --all
-        fi
-    fi
-    # check if specified branch exists
-    if [ -n "$BRANCH" ]; then
-        if ! git branch -r | grep -wq "$BRANCH"; then
-            _war "branch \"$BRANCH\" not found"
-            continue
-        fi
-        # checkout the specified branch
-        git checkout "$BRANCH"
-        # pull latest changes
-        git pull
-    fi
-    if [ -n "$TAG" ]; then
-        if ! git tag | grep -xq "$TAG"; then
-            _war "tag \"$TAG\" not found"
-            continue
-        fi
-        # checkout the specified tag
-        git checkout "$TAG"
-    fi
-done
-
-# go back to calling directory
-popd >/dev/null || exit 1
